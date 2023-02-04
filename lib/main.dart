62c39e60d27291f4f834e7e73133a423255e9f94
EssamEzi <essamaa119@gmail.com>
2023-02-05 01:29:12 +0300

    Send  Handling Notification  Foreground Background

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,49 @@
-// ignore_for_file: non_constant_identifier_names, unused_local_variable
+// ignore_for_file: non_constant_identifier_names, unused_local_variable, avoid_print
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:media/cubits/Layout/cubit.dart';
 import 'package:media/layout/media_layout.dart';
 import 'package:media/modules/media_login_screen.dart';
 import 'package:media/shared/Bloc_obServer.dart';
+import 'package:media/shared/components/components.dart';
 import 'package:media/shared/components/constants.dart';
 import 'package:media/shared/network/local.dart';
 import 'package:media/shared/styles/themes.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  //التعامل مع الاشعارات
+  print('onMessageOpenedApp');
+  print(message.data.toString());
+  ShowToast(text: 'onMessageOpenedApp', states: ToastStates.SUCCESS);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = MyBlocObserver();
 //////// start check open Layout /////
+/////خاص التعامل مع الاشعارات
+  var token = await FirebaseMessaging.instance.getToken();
+  print('rtootgm $token');
+  //االتعامل مع الاشعار وانت فاتح التطبيق
+  FirebaseMessaging.onMessage.listen((event) {
+    print('onMessage');
+    print(event.data.toString());
+    ShowToast(text: 'on Message', states: ToastStates.SUCCESS);
+  });
+  // في حاله الضغط على الاشعار رح ينقلنا الى اي مكان نريده
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print('onMessageOpenedApp');
+    print(event.data.toString());
+    ShowToast(text: 'onMessageOpenedApp', states: ToastStates.SUCCESS);
+  });
+  // Backend الاشعارات
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  ///end الاشعارات
 
   await CacheHelper.init();
   uId = CacheHelper.getData(key: 'uId');
